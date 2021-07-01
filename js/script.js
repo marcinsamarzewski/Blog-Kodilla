7de9e2cfcbd202da2883da99a6ec250f128b1f7e
Marcin Samarzewski <m.samarzewski1990@gmail.com>
2021-07-01 20:48:22 +0200

    add javascript code

--- js/script.js
+++ js/script.js
@@ -24,13 +24,16 @@ function titleClickHandler(event){
     activeArticle.classList.remove('active');
   }
 
-  /* get 'href' attribute from the clicked link */
+  /* [DONE] get 'href' attribute from the clicked link */
   const articleSelector = clickedElement.getAttribute('href')
   console.log(articleSelector)
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector(articleSelector)
+  console.log(targetArticle)
 
-  /* add class 'active' to the correct article */
+  /* add cla[DONE] ss 'active' to the correct article */
+  targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
